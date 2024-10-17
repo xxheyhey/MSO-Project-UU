b5e2a6a64809ad83f12984bac384651df7fdef70
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-10-17 08:45:30 +0200

    added namespaces, etc.

--- MSO_Project/MSO_Project/Program.cs
+++ MSO_Project/MSO_Project/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace MSO_Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- MSO_Project/Test_MSO_Project/UnitTest1.cs
+++ MSO_Project/Test_MSO_Project/UnitTest1.cs
@@ -1,9 +1,10 @@
-namespace Test_MSO_Project;
-
-public class UnitTest1
+namespace Test_MSO_Project
 {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
+        [Fact]
+        public void Test1()
+        {
+        }
     }
 }
